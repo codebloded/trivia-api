6fd9db3a9171f1d54e2b9e5d0461ddaf6abc3e92
Surya Kant Bansal <suryakantbansal97@gmail.com>
2019-07-29 00:13:30 +0530

    implement all apis

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,7 +1,11 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
+import random
+import re
 
-from backend.database.models import setup_db, Category
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from werkzeug.exceptions import HTTPException
+
+from backend.database.models import setup_db, Category, Question
 
 QUESTIONS_PER_PAGE = 10
 
@@ -33,10 +37,14 @@ def create_app(test_config=None):
     for all available categories.
     '''
 
-    @app.route('/categories')
+    @app.route('/categories', methods=['GET'])
     def get_all_categories():
-        categories = [category.format() for category in Category.query.all()]
-        return jsonify(categories)
+        categories = {}
+        for category in Category.query.all():
+            categories[category.id] = category.type
+        return jsonify({
+            'categories': categories
+        })
 
     '''
     @TODO: 
@@ -51,6 +59,21 @@ def create_app(test_config=None):
     Clicking on the page numbers should update the questions. 
     '''
 
+    @app.route('/questions', methods=['GET'])
+    def get_questions():
+        categories = {}
+        for category in Category.query.all():
+            categories[category.id] = category.type
+        questions = [question.format() for question in Question.query.all()]
+        page = int(request.args.get('page', '0'))
+        upper_limit = page * 10
+        lower_limit = upper_limit - 10
+        return jsonify({
+            'questions': questions[lower_limit:upper_limit] if page else questions,
+            'total_questions': len(questions),
+            'categories': categories
+        })
+
     '''
     @TODO: 
     Create an endpoint to DELETE question using a question ID. 
@@ -58,6 +81,14 @@ def create_app(test_config=None):
     TEST: When you click the trash icon next to a question, the question will be removed.
     This removal will persist in the database and when you refresh the page. 
     '''
+
+    @app.route('/questions/<int:question_id>', methods=['DELETE'])
+    def delete_question(question_id):
+        question = Question.query.get(question_id)
+        question.delete()
+        return jsonify({
+            'deleted': question_id
+        })
 
     '''
     @TODO: 
@@ -70,6 +101,19 @@ def create_app(test_config=None):
     of the questions list in the "List" tab.  
     '''
 
+    @app.route('/questions', methods=['POST'])
+    def post_question():
+        question = Question(
+            question=request.json['question'],
+            answer=request.json['answer'],
+            difficulty=request.json['difficulty'],
+            category=request.json['category']
+        )
+        question.insert()
+        return jsonify({
+            'question': question.format()
+        })
+
     '''
     @TODO: 
     Create a POST endpoint to get questions based on a search term. 
@@ -81,6 +125,17 @@ def create_app(test_config=None):
     Try using the word "title" to start. 
     '''
 
+    @app.route('/search', methods=['POST'])
+    def search():
+        search_term = request.json['searchTerm']
+        all_questions = Question.query.all()
+        questions = [question.format() for question in all_questions if
+                     re.search(search_term, question.question, re.IGNORECASE)]
+        return jsonify({
+            'questions': questions,
+            'total_questions': len(all_questions)
+        })
+
     '''
     @TODO: 
     Create a GET endpoint to get questions based on category. 
@@ -89,6 +144,17 @@ def create_app(test_config=None):
     categories in the left column will cause only questions of that 
     category to be shown. 
     '''
+
+    @app.route('/categories/<int:category_id>/questions', methods=['GET'])
+    def get_questions_by_category(category_id):
+        category = Category.query.get(category_id)
+        questions = [question.format() for question in Question.query.all() if
+                     question.category == category.id]
+        return jsonify({
+            'questions': questions,
+            'total_questions': len(questions),
+            'current_category': category_id
+        })
 
     '''
     @TODO: 
@@ -102,10 +168,53 @@ def create_app(test_config=None):
     and shown whether they were correct or not. 
     '''
 
+    @app.route('/quizzes', methods=['POST'])
+    def get_quiz_questions():
+        previous_questions = request.json['previous_questions']
+        quiz_category = request.json['quiz_category']
+        all_questions = Question.query.all()
+        questions = [question.format() for question in all_questions if
+                     question.category == int(quiz_category['id']) or not quiz_category['id']]
+        if len(previous_questions) == len(questions):
+            return jsonify({})
+        question = random.choice(questions)
+        while any(question_id == question['id'] for question_id in previous_questions):
+            question = random.choice(questions)
+        return jsonify({
+            'question': question
+        })
+
     '''
     @TODO: 
     Create error handlers for all expected errors 
     including 404 and 422. 
     '''
+
+    # Error Handler
+    @app.errorhandler(HTTPException)
+    def http_exception_handler(error):
+        """
+        HTTP error handler for all endpoints
+        :param error: HTTPException containing code and description
+        :return: error: HTTP status code, message: Error description
+        """
+        return jsonify({
+            'success': False,
+            'error': error.code,
+            'message': error.description
+        }), error.code
+
+    @app.errorhandler(Exception)
+    def exception_handler(error):
+        """
+        Generic error handler for all endpoints
+        :param error: Any exception
+        :return: error: HTTP status code, message: Error description
+        """
+        return jsonify({
+            'success': False,
+            'error': 500,
+            'message': f'Something went wrong: {error}'
+        }), 500
 
     return app
